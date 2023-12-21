8546028a76165ab2a05910422548b7b0479d5915
arwaa29 <arwahesham73@gmail.com>
2023-12-21 03:59:53 +0200

    alx 0x06.python

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Square module."""
+
+
+class Square:
+    """Defines a aquare."""
+
+    def __init__(self, size):
+        """constructor.
+
+
+        Args:
+            size: length of a side o th square
+        """
+        self.__size = size
